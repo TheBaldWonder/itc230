d404ad7a24d9986070aaf44ade5157c4cd16e6d5
TheBaldWonder <heilman.jonathan83@gmail.com>
2019-07-01 20:19:25 -0700

    Asignment 2 Completed
    This commit completes Assignment 2

--- index.js
+++ index.js
@@ -1,31 +1,51 @@
 var http = require('http'),        
-      fs = require('fs');
+      fs = require('fs'),
+      qs = require('querystring'),
+   books = require('./lib/books');
 
 function serveStaticFile(res, path, contentType, responseCode) {        
     if(!responseCode) responseCode = 200;        
     fs.readFile(__dirname + path, function(err,data) {                
         if(err) {                        
-            res.writeHead(500, { 'Content-Type': 'text/plain' });                       
-            res.end('500 - Internal Error');                
+            res.writeHead(500, { 'Content-Type': 'text/plain' });                       res.end('500 - Internal Error');                
         } else {                        
-            res.writeHead(responseCode, { 'Content-Type': contentType });                 
-            res.end(data);                
+            res.writeHead(responseCode, { 'Content-Type': contentType });                 res.end(data);                
         }        
     }); 
 }
 
-http.createServer(function(req,res){                
-    var path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();        
+http.createServer(function(req,res){  
+    var url = req.url.split("?"); 
+    var params = qs.parse(url[1]); 
+    var path = url[0].toLowerCase(); 
+    
     switch(path) {                
-        case '':                        
-            serveStaticFile(res, '/public/home.html', 'text/html');                       
-            break;                
+        case '/':                        
+            serveStaticFile(res, '/public/home.html', 'text/html');                       break;                
         case '/about':                        
-            serveStaticFile(res, '/public/about.html', 'text/html');                     
-            break;                             
+            serveStaticFile(res, '/public/about.html', 'text/html');                     break;         
+        case '/add':
+            res.writeHead(200, { 'Content-Type': 'text/plain' });                       res.end('Add');   
+            break;
+        case '/search':
+            if(!(books.get(params.title))){
+                res.writeHead(200, { 'Content-Type': 'text/plain' });
+                res.end('No records found for: ' + params.title);
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/plain' });
+                res.end(JSON.stringify(books.get(params.title)));
+            } 
+            break;
+        case '/remove':
+            if(!(books.get(params.title))){
+                res.writeHead(200, { 'Content-Type': 'text/plain' });
+                res.end("No records found for: " + params.title);
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/plain' });
+                res.end(JSON.stringify(books.remove(params.title)));
+            }                  
         default: 
-            serveStaticFile(res, '/public/notfound.html', 'text/html', 404);                   
-            break;        
+            serveStaticFile(res, '/public/notfound.html', 'text/html', 404);             break;        
     } 
 }).listen(3000);
 
