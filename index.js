8b0f91804649814db61b5b341b0391bc1cb92d29
TheBaldWonder <heilman.jonathan83@gmail.com>
2019-07-10 16:15:16 -0700

    Assignemnt 3

--- index.js
+++ index.js
@@ -1,52 +1,85 @@
-var http = require('http'),        
-      fs = require('fs'),
-      qs = require('querystring'),
-   books = require('./lib/books');
+'use strict'
 
-function serveStaticFile(res, path, contentType, responseCode) {        
-    if(!responseCode) responseCode = 200;        
-    fs.readFile(__dirname + path, function(err,data) {                
-        if(err) {                        
-            res.writeHead(500, { 'Content-Type': 'text/plain' });                       res.end('500 - Internal Error');                
-        } else {                        
-            res.writeHead(responseCode, { 'Content-Type': contentType });                 res.end(data);                
-        }        
-    }); 
-}
+var       http = require('http'),        
+            fs = require('fs'),
+            qs = require('querystring'),
+         books = require('./lib/books');
 
-http.createServer(function(req,res){  
-    var url = req.url.split("?"); 
-    var params = qs.parse(url[1]); 
-    var path = url[0].toLowerCase(); 
+const express = require("express");
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.use(express.static(__dirname + '/public')); // set location for static files
+app.use(require("body-parser").urlencoded({extended: true}));
+
+let handlebars = require("express-handlebars");
+app.engine('.handlebars', handlebars({extname: '.handlebars'}));
+app.set("view engine", ".handlebars");
+
+
+// send content of 'home' view
+app.get('/', function(req,res){
+    res.render('home', {siteName: "The Movie Database"});
+});
+
+app.get('/about', function(req,res){
+    res.render('about');
+});
+
+app.get('/img/logo.jpg', function(req,res){
+    res.render('logo.jpg');
+});
+
+app.post('/search', function(req,res){
+    let result = books.get(req.body.title);
     
-    switch(path) {                
-        case '/':                        
-            serveStaticFile(res, '/public/home.html', 'text/html');                       break;                
-        case '/about':                        
-            serveStaticFile(res, '/public/about.html', 'text/html');                     break;         
-        case '/add':
-            res.writeHead(200, { 'Content-Type': 'text/plain' });                       res.end('Add');   
-            break;
-        case '/search':
-            if(!(books.get(params.title))){
-                res.writeHead(200, { 'Content-Type': 'text/plain' });
-                res.end('No records found for: ' + params.title);
-            } else {
-                res.writeHead(200, { 'Content-Type': 'text/plain' });
-                res.end(JSON.stringify(books.get(params.title)));
-            } 
-            break;
-        case '/remove':
-            if(!(books.get(params.title))){
-                res.writeHead(200, { 'Content-Type': 'text/plain' });
-                res.end("No records found for: " + params.title);
-            } else {
-                res.writeHead(200, { 'Content-Type': 'text/plain' });
-                res.end(JSON.stringify(books.remove(params.title)));
-            }                  
-        default: 
-            serveStaticFile(res, '/public/notfound.html', 'text/html', 404);             break;        
+    if(!(books.get(req.body.title))){
+        res.send('Searching for: ' + req.body.title + '<br>' + 
+                 'No records found for: '  + req.body.title);
+    } else {
+        res.render('details', {title: req.body.title, result: result });
     } 
-}).listen(3000);
+});
 
-console.log('Server started on localhost:3000; press Ctrl-C to terminate....');
+app.post('/remove', function(req,res){
+   if(!(books.get(req.body.title))){
+        res.send("No records found for: " + req.body.title);
+    } else {
+//        res.send(JSON.stringify(books.remove(req.body.title)));
+        res.send(books.remove(req.body.title));
+    }    
+});
+
+app.post('/add', function(req,res){
+    var newBook = {
+        title: req.body.title,
+        author: req.body.author,
+        pubdate: req.body.pubdate
+    };
+    
+    if(books.get(req.body.title)){
+        res.send("Title is already in the collection: " + req.body.title);
+    } else {
+//        res.send(books.add(req.body.title, req.body.author, req.body.pubdate));
+        res.send(books.add(newBook));
+    }    
+});
+
+app.get('/headers', function(req,res){
+   res.set('Content-Type','text/plain');    
+   var s = '';    
+   for(var name in req.headers) s += name + ': ' + req.headers[name] + '\n';    
+   res.send(s);  
+});
+
+
+
+
+app.use(function(req,res){
+    res.status(404).render('404');
+});
+
+
+app.listen(app.get('port'), function() {
+ console.log('Express started'); 
+});
