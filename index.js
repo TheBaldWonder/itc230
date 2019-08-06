3c1956a93803f18d9fc3608f387c3f32d1242007
TheBaldWonder <heilman.jonathan83@gmail.com>
2019-08-05 19:05:54 -0700

    Assignment 7 Complete
    Remodeled my app and updated to SPA

--- index.js
+++ index.js
@@ -1,105 +1,101 @@
-'use strict'
+'use strict';
 
-var     books = require('./lib/books');
+let express = require("express");
+let bodyParser = require("body-parser");
+let Book = require("./models/book"); // use database model
 
-const express = require("express");
-const app = express();
+let app = express();
 
+// configure Express app
 app.set('port', process.env.PORT || 3000);
-app.use(express.static(__dirname + '/public')); // set location for static files
-app.use(require("body-parser").urlencoded({extended: true}));
-
-let handlebars = require("express-handlebars");
-app.engine('.handlebars', handlebars({extname: '.handlebars'}));
-app.set("view engine", ".handlebars");
-
-//var mongoose = require('mongoose');
-//mongoose.connect('mongodb://localhost/test');
-
-var Book = require("./models/Book.js");
-
-new Book({title: "Fake123", author: "John Fake", pubdate: 1999}).save(); 
-
-// return all records
-Book.find({}, function (err, items) {
-  if (err) return next(err);
-  console.log(items.length);
-  // other code here
+app.use(express.static(__dirname + '/../public'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+//app.use('/api', require("cors")());
+app.use((err, req, res, next) => {
+  console.log(err);
 });
 
+// set template engine
+let handlebars =  require("express-handlebars");
+app.engine(".html", handlebars({extname: '.html'}));
+app.set("view engine", ".html");
 
-// send content of 'home' view
-app.get('/', function(req,res){
-    res.render('home', {siteName: "The Book Database", titles: ["The Great Gatsby", "Ulysses", "Moby Dick", "War and Peace", "Great Expectations"]});
+app.get('/', (req,res, next) => {
+    Book.find((err,books) => {
+        if (err) return next(err);
+        res.render('home', {books: JSON.stringify(books)});    
+    });
 });
 
-app.get('/all', function(req,res){
-    console.log(books.all());
+app.get('/about', (req,res) => {
+    res.type('text/html');
+    res.render('about');
 });
 
-app.get('/about', function(req,res){
-    res.render('about', {siteName: "The Book Database"});
+// api's
+app.get('/api/v1/book/:title', (req, res, next) => {
+    let title = req.params.title;
+    Book.findOne({title: title}, (err, result) => {
+        if (err || !result) return next(err);
+        res.json( result );    
+    });
 });
 
-app.get('/img/logo.jpg', function(req,res){
-    res.render('logo.jpg');
+app.get('/api/v1/books', (req,res, next) => {
+    Book.find((err,results) => {
+        if (err || !results) return next(err);
+        res.json(results);
+    });
 });
 
-app.post('/search', function(req,res){
-      let result = req.body.title;    
-    
-      Book.findOne({"title": result}, function(err, books){
-      if(err){
-          return next(err);
-      } 
-      if(!books){
-          return next();
-      }
-      
-//      res.type('text/html');
-      res.render('details', {title: books.title, result: books});
-      console.log(books);
-      }); 
+app.get('/api/v1/delete/:id', (req,res, next) => {
+    Book.remove({"_id":req.params.id }, (err, result) => {
+        if (err) return next(err);
+        // return # of items deleted
+        //res.json({"deleted": result.result.n});
+        res.json({"deleted  ": result.n});
+        // console.log({"deleted": result});
+        console.log("       \*  ");
+        console.log("       \*   ");
+        console.log("       \*   ");
+        console.log("       \*   ");
+        console.log("deleted   " + result.n);
+    });
 });
 
-app.post('/remove', function(req,res){
-    var result = books.remove(req.body.title);
-    
-   if(!result.deleted){
-        res.send("No records found for: " + req.body.title);
-    } else {
-        res.render('deleted', {title: req.body.title});
-    }    
+app.post('/api/v1/add/', (req,res, next) => {
+    // find & update existing item, or add new 
+    if (!req.body._id) { // insert new document
+        let book = new Book({title:req.body.title,director:req.body.director,releasedate:req.body.releasedate});
+        book.save((err,newBook) => {
+            if (err) return next(err);
+            res.json({updated: 0, _id: newBook._id});
+        });
+    } else { // update existing document
+        Book.updateOne({ _id: req.body._id}, {title:req.body.title, director: req.body.director, releasedate: req.body.releasedate }, (err, result) => {
+            if (err) return next(err);
+            res.json({updated: result.nModified, _id: req.body._id});
+        });
+    }
 });
 
-app.post('/add', function(req,res){
-    var newBook = {
-        title: req.body.title,
-        author: req.body.author,
-        pubdate: req.body.pubdate
-    };
-    var result = books.add(newBook);
-
-    if(!result.added){
-        res.send("Title is already in the collection: " + req.body.title);
-    } else {
-        res.send("Title added");
-    }    
+app.get('/api/v1/add/:title/:director/:releasedate', (req,res, next) => {
+    // find & update existing item, or add new 
+    let title = req.params.title;
+    Book.update({ title: title}, {title:title, director: req.params.director, releasedate: req.params.releasedate }, {upsert: true }, (err, result) => {
+        if (err) return next(err);
+        // nModified = 0 for new item, = 1+ for updated item 
+        res.json({updated: result.nModified});
+    });
 });
 
-app.get('/headers', function(req,res){
-   res.set('Content-Type','text/plain');    
-   var s = '';    
-   for(var name in req.headers) s += name + ': ' + req.headers[name] + '\n';    
-   res.send(s);  
+app.use((req,res) => {
+    res.type('text/plain'); 
+    res.status(404);
+    res.send('404 - Not found');
 });
 
-
-app.use(function(req,res){
-    res.status(404).render('404');
-});
-
-
-app.listen(app.get('port'), function() {
- console.log('Express started'); 
+app.listen(app.get('port'), () => {
+    console.log('Express started');    
 });
